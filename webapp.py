cb5b1359d7bd8a1f91f6a99c6c265b6d58e645cc
omiete1 <105213543+omiete01@users.noreply.github.com>
2025-06-08 14:18:07 +0100

    Docker push

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello DevOps!\n"
+    return "Hello DevOps!!!\n"
 
 if __name__ in "__main__":
     app.run(host='0.0.0.0', port=5000)
