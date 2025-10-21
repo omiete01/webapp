92330c9d43468c4d604270a14e5465c6a6b668f1
omiete01 <estheromiete01@gmail.com>
2025-10-21 21:53:02 +0100

    self-hosted runner update

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello there, DevOps!!!\n"
+    return "Hello there, DevOps!!!"
 
-if __name__ in "__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
